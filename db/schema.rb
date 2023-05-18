2a975544f4bc9b20eec0c8c731feed0e3e25b031
phuocbui <phuocbui@kms-technology.com>
2023-05-18 15:09:22 +0700

    add association between user and collection

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_18_074407) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_18_075748) do
   create_table "books", force: :cascade do |t|
     t.string "name"
     t.string "author"
@@ -22,6 +22,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_18_074407) do
     t.string "fileURL"
   end
 
+  create_table "collections", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_collections_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
@@ -29,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_18_074407) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "collections", "users"
 end
